f20f8bb86e5289992939ced93de75f2263e6166f
DeuklyoungKo <deutschgon@gmail.com>
2019-05-18 01:38:54 +0200

    chapter 3 (https://symfonycasts.com/screencast/javascript-webpack)

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './web/assets/js/RepLogApp.js',
+    output: {
+        path: path.join( __dirname, 'web','build'),
+        filename: 'rep_log.js',
+    }
+};
