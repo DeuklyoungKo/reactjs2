131640faf622f6cfc7446d4232de6efc47954cf2
DeuklyoungKo <deutschgon@gmail.com>
2019-05-19 10:39:22 +0200

    chapter 7 (https://symfonycasts.com/screencast/javascript-webpack)

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,14 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './web/assets/js/rep_log.js',
+    entry: {
+        rep_log: './web/assets/js/rep_log.js',
+        login: './web/assets/js/login.js',
+        layout: './web/assets/js/layout.js',
+    },
     output: {
         path: path.join( __dirname, 'web','build'),
-        filename: 'rep_log.js',
+        filename: '[name].js',
     },
     watchOptions: {
         poll: true
